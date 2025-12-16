df7bc2c24e6c4be429909315b036b3fe47a4dc08
AlbadiFTW <projectekko1962@gmail.com>
2025-12-16 15:51:51 +0400

    Fix: Configure Convex Auth to use bcrypt instead of Scrypt for password hashing

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,10 +1,25 @@
+"use node";
+
 import { convexAuth, getAuthUserId } from "@convex-dev/auth/server";
 import { Password } from "@convex-dev/auth/providers/Password";
 import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
 import { query } from "./_generated/server";
+import bcrypt from "bcryptjs";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Password, Anonymous],
+  providers: [
+    Password({
+      crypto: {
+        async hashSecret(secret: string): Promise<string> {
+          return await bcrypt.hash(secret, 10);
+        },
+        async verifySecret(secret: string, hash: string): Promise<boolean> {
+          return await bcrypt.compare(secret, hash);
+        },
+      },
+    }),
+    Anonymous,
+  ],
 });
 
 export const loggedInUser = query({
